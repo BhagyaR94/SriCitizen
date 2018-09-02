dd5668f7c52768fc3191ab506d342c0e5d8a29da
Bhagya Rathnayake <bhagya@omobio.net>
2018-09-02 12:26:24 +0530

    Card COmponent added

---
+++ src/views/components/CardComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+class CardComponent extends Component {
+
+    render() {
+        return (
+            <View>
+                <View>
+                    <Image />
+                </View>
+                <View>
+                    <Text></Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default CardComponent;
