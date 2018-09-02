6a0d11ac7bf216b5f10df12353e8c79ff8afc6a7
Bhagya Rathnayake <bhagya@omobio.net>
2018-09-02 13:29:43 +0530

    navigation related codes added in tile components

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -111,17 +111,20 @@ class HomeScreen extends Component {
 
                             <TileComponent
                                 btnText={'Sample 123 here'}
+                                onPress={this.gotoScreen('sample1', 'sample1')}
                             />
 
                             <TileComponent
                                 btnText={'Sample 123 here'}
+                                onPress={this.gotoScreen('sample1', 'sample1')}
                             />
 
                             <TileComponent
                                 btnText={'Sample 123 here'}
-                            /> 
+                                onPress={this.gotoScreen('sample1', 'sample1')}
+                            />
                             {/* sample */}
-                            
+
                         </View>
                         {/* End of First Row */}
 
